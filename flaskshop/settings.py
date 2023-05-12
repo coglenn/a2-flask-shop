e9ed017e81567b55b48402381028ef488683e5ef
coglenn <codagl@msn.com>
2023-05-11 21:47:29 -0600

    local to prod updates

--- flaskshop/settings.py
+++ flaskshop/settings.py
@@ -78,7 +78,7 @@ class Config:
     PURCHASE_URI = os.getenv("PURCHASE_URI", "")
 
     BCRYPT_LOG_ROUNDS = 13
-    DEBUG_TB_ENABLED = os.getenv("FLASK_DEBUG", True)  # Disable Debug toolbar
+    DEBUG_TB_ENABLED = os.getenv("FLASK_DEBUG", False)  # Disable Debug toolbar
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     DEBUG_TB_PROFILER_ENABLED = True
 
