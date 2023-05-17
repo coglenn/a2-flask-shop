b8a14a9cd098a7fa69f8a950f0ef730da8814f85
coglenn <codagl@msn.com>
2023-05-16 21:41:46 -0600

    lib

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,9 @@
-import importlib
+import imp
 import os
 import sys
 
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-wsgi = importlib.load_source('wsgi', 'app.py')
+wsgi = imp.load_source('wsgi', 'app.py')
 application = wsgi.app
