74f27d18ee1b92c885d528aa911a6224a2bc227c
coglenn <codagl@@msn.com>
2023-11-22 09:29:46 -0700

    test cutomer address

--- flaskshop/order/views.py
+++ flaskshop/order/views.py
@@ -208,7 +208,7 @@ def payment_success(token):
     line_items = OrderLine.query.filter_by(order_id=order.id)
     get_usr_address = UserAddress.query.filter_by(user_id=order_user_id).first()
     cust = stripe.Customer.retrieve(str(current_user.id))
-    print(cust)
+    print(cust['address'])
     order_json = {
         "recipient": {
             "name": get_usr_address.contact_name,
